56bc88b46b262e64d77ba3539806d1e5b106fb89
yrom1 <ryanm.inbox@gmail.com>
2022-02-01 04:02:03 -0500

    Basic prompt

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #define FMT_HEADER_ONLY
+#include <bits/stdc++.h>
+
 #include "/home/ryan/Downloads/fmt/include/fmt/format.h"
 
-int main() { fmt::print("The answer is {}.\n", 42); }
+int main() {
+  fmt::print("lisp version 0.0.1\n");
+  fmt::print("ctrl + c to exit\n\n");
+  while (true) {
+    fmt::print("lisp> ");
+    std::string input;
+    std::getline(std::cin, input);
+    fmt::print("you said {}!\n", input);
+  }
+}
